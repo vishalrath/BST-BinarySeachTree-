4894aa3c9bc44d01cedb9166149559e9b3a58e1c
vishal <vishalrathod7038@gmail.com>
2021-11-02 07:41:12 +0530

    [Vishal] Creat BinarySearchTree and adding Number

---
+++ BSTUc1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BSTUc1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            BinarySearchTree<int> bst = new BinarySearchTree<int>(56);
+            bst.Insert(30);
+            bst.Insert(70);
+            bst.display();
+
+        }
+    }
+}
